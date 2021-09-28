1d7afd1ca0f363dea9dd073dfef276fe36ac4236
Rastra17 <rastradhoj@gmail.com>
2021-09-28 13:49:30 +0545

    completed connect

--- src/ms/Connect.java
+++ src/ms/Connect.java
@@ -14,4 +14,21 @@ public class Connect
       Class.forName("com.mysql.cj.jdbc.Driver");
       String Url="jdbc:mysql://localhost:3306/ms";
       String User="root";
-      
+      String Password="root";
+      Connection db=DriverManager.getConnection(Url,User,Password);
+
+      Statement st=db.createStatement();
+      st.executeUpdate("CREATE TABLE IF NOT EXISTS customer_details(user_id INTEGER AUTO_INCREMENT Primary Key,fullname VARCHAR(255),username VARCHAR(255),email VARCHAR(255),password VARCHAR(255),address VARCHAR(255),contact VARCHAR(255));");
+      db.close();
+
+    }
+    catch(Exception e)
+    {
+      System.out.println(e);
+    }
+    finally
+    {
+      System.out.println("Closed database");
+    }
+  }
+}
